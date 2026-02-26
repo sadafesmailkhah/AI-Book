8f3c5c4ba379eded380435c63edda5d7e82cb5d3
sadaf-84 <sadafe973@gmail.com>
2026-02-26 08:32:12 +0330

    addin stats in py

--- stats.py
+++ stats.py
@@ -1,1 +1,24 @@
-#just a place holder
+import os
+
+def get_file_size(file_path):
+    try:
+        size_bytes = os.path.getsize(file_path)
+        size_kb = size_bytes / 1024
+        size_mb = size_kb / 1024
+
+        return {
+            "bytes": size_bytes,
+            "kilobytes": round(size_kb, 2),
+            "megabytes": round(size_mb, 2)
+        }
+
+    except FileNotFoundError:
+        return "File not found."
+    except Exception as e:
+        return f"Error: {e}"
+
+
+# Example usage
+file_info = get_file_size("example.txt")
+print(file_info)
+>>>>>>> d0e603d (addin stats in py)
